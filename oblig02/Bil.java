e4398e4fb3809d67cd3969f62c3fef39c6171f5e
Kenneth Ravn <kennethravn@1x-193-157-182-22.uio.no>
2017-02-16 12:29:52 +0100

    noe

--- oblig02/Bil.java
+++ oblig02/Bil.java
@@ -6,15 +6,6 @@ public class Bil {
         this.kjennemerke = kjennemerke;
     }
 
-    public void setjennemerke(String kjennemerke) {
-        this.kjennemerke = kjennemerke;
-
-    }
-
-    public String getKjennemerke() {
-        return kjennemerke;
-    }
-
     public void getInfo() {
 	System.out.printf("Kjennemerke: %s\n", kjennemerke);
     }
